58d5532b8bad7a307b385cc7815a6bb1cc17d4fd
michal <michalbork3@gmail.com>
2023-12-09 14:20:23 +0100

    Invalid dir name

--- controllers/front/widget.php
+++ controllers/front/widget.php
@@ -1,6 +1,6 @@
 <?php
 
-class Go_freedeliveryWidgetModuleFrontController extends ModuleFrontController
+class go_freedeliveryWidgetModuleFrontController extends ModuleFrontController
 {
     public function initContent()
     {
